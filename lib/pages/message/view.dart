814ab4fed1cb4bfd07d4725e96dcccaeffaa50a6
Mohammad Alipour <alipour@billiger-mietwagen.de>
2023-08-08 00:47:29 +0200

    syncing user data with server

--- lib/pages/message/view.dart
+++ lib/pages/message/view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foton/common/routes/routes.dart';
 import 'package:get/get.dart';
 
 import '../../common/values/colors.dart';
@@ -75,7 +76,35 @@ class MessagePage extends GetView<MessageController> {
               pinned: true,
               title: _headBar(),
             )
-          ])
+          ]),
+          Positioned(
+              right: 20.w,
+              bottom: 70.w,
+              width: 50.w,
+              height: 50.w,
+              child: GestureDetector(
+            child: Container(
+              height: 50.w,
+              width: 50.w,
+              padding: EdgeInsets.all(10.w),
+              decoration: BoxDecoration(
+                color: AppColors.primaryElement,
+                borderRadius: BorderRadius.all(Radius.circular(40.w)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    offset: const Offset(1,1)
+                  ),
+                ]
+              ),
+              child: Image.asset("assets/icons/contact.png"),
+            ),
+                onTap: (){
+                  Get.toNamed(AppRoutes.Contact);
+                },
+          ))
         ],
       )),
     );
